32740e4e768ad9e3ffbf7ef58b58e8bec5e57af5
Wayne <wayne.cen@gmail.com>
2022-06-27 04:29:44 -0400

    Add StartPage, update favicon, add styling
    Add options for Quiz customization on StartPage
    
    Add button component and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./style.css";
+import StartPage from "./StartPage";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<StartPage />
+		</div>
+	);
 }
 
 export default App;
